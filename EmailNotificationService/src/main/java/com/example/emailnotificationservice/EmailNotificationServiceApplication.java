40b2ee6b930f1765466672e9079020ec6aada2e8
Osama Rimawi <osamarimawi2002@gmail.com>
2023-09-07 16:39:21 +0300

    dockrizing the project

--- EmailNotificationService/src/main/java/com/example/emailnotificationservice/EmailNotificationServiceApplication.java
+++ EmailNotificationService/src/main/java/com/example/emailnotificationservice/EmailNotificationServiceApplication.java
@@ -6,10 +6,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
 @Slf4j
+@EnableDiscoveryClient
 public class EmailNotificationServiceApplication {
 
     @Autowired
